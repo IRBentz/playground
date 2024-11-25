b87f7c160eecb176edf3c8b47212ea9cc0756569
IRBentz <168255936+IRBentz@users.noreply.github.com>
2024-11-25 14:54:31 -0500

    Adding Package and Runner components

---
+++ 0/src/main/java/code_package/Runner.java
@@ -0,0 +1,14 @@
+package code_package;
+
+public class Runner {
+
+	public Runner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
