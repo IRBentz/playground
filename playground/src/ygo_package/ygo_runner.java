23163fdd6498020bd4cedbdbff41da2befe1525b
IRBentz <168255936+IRBentz@users.noreply.github.com>
2024-11-28 15:27:47 -0500

    Renamed .data files

--- playground/src/ygo_package/ygo_runner.java
+++ playground/src/ygo_package/ygo_runner.java
@@ -4,8 +4,8 @@ package ygo_package;
 public class ygo_runner {
 
 	public ygo_runner() {
-		String db_fileName = "src//ygo_package//database.data";
-		String fal_fileName = "src//ygo_package//forbidden&limited_list.data";
+		String db_fileName = "src//ygo_package//card.data";
+		String fal_fileName = "src//ygo_package//fal.data";
 		Backend.start(db_fileName, fal_fileName);
 	}
 
