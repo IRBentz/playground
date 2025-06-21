9e6396c044c0fb9109c7a2d98a75090ab71a9e50
IRBentz <168255936+IRBentz@users.noreply.github.com>
2025-06-21 14:50:09 -0400

    Adding Card BG Render Tester

--- playground/src/ygo_eng/data/DataLoadTester.java
+++ playground/src/ygo_eng/data/DataLoadTester.java
@@ -1,3 +1,4 @@
+
 package ygo_eng.data;
 
 import java.io.File;
--- playground/src/ygo_eng/ui/CardUI.java
+++ playground/src/ygo_eng/ui/CardUI.java
@@ -1,38 +1,63 @@
 package ygo_eng.ui;
 
-import java.awt.Dimension;
+import java.awt.Image;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
 
 public class CardUI extends JFrame{
 	
 	private static final long serialVersionUID = 8856255655978002135L;
-	
+	private static ArrayList<ImageIcon> cardBG_II = new ArrayList<>();
 	private JPanel cardPanel;
+	private int factor;
 	
 	public CardUI() {
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		final int width = 59 * 5;
-		final int height = 86 * 5;
-		Dimension windowDim = new Dimension(width, height);
-		setPreferredSize(windowDim);
-		setMinimumSize(windowDim);
-		setMaximumSize(windowDim);
-		
+		factor = 4;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//ImageIcon ii = new ImageIcon("src//ygo_eng//card//card_png//effect.png");
 		cardPanel = new JPanel();
-		cardPanel.setPreferredSize(windowDim);
-		cardPanel.setMinimumSize(windowDim);
-		cardPanel.setMaximumSize(windowDim);
 		add(cardPanel);
-		cardPanel.setBackground(Card_bg.EFFECT.bg_c);
-		
+		//JLabel jl = new JLabel(ii);
+		//cardPanel.add(jl);
+		pack();
 		setVisible(true);
 	}
 	
+	public void setCardBG(ImageIcon image) {
+		cardPanel.add(new JLabel(new ImageIcon(image.getImage().getScaledInstance(59 * factor, 86 * factor, Image.SCALE_SMOOTH))));
+		pack();
+	}
+	
+	public static void buildCardBG_II(String pointerFileName) {
+		Scanner pointerFileScanner;
+		try {
+			pointerFileScanner = new Scanner(new File(pointerFileName));
+			System.out.println("Found file at: " + new File(pointerFileName).getPath());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		String filePath = pointerFileScanner.nextLine();
+		System.out.println("Successfully found designated file path at: " + filePath.replace("//", "\\"));
+		while (pointerFileScanner.hasNext()) {
+			cardBG_II.add(new ImageIcon(filePath + pointerFileScanner.nextLine()));
+		}
+		
+		pointerFileScanner.close();
+	}
+	
 	public static void main (String args[]) {
-		new CardUI();
+		//new CardUI();
+		CardUI.buildCardBG_II("src//ygo_eng//ui//png_pointers.txt");
+		Arrays.stream(cardBG_II.toArray()).forEach(image -> new CardUI().setCardBG((ImageIcon) image));
 	}
 
 }
